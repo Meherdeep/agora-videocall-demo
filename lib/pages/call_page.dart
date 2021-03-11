5cac9927c281bd131cc62cdde44d18e27114598d
Sai Sandeep Rayanuthala <raynuthalas@gmail.com>
2021-03-11 19:54:58 +0530

    ⏫   Upgraded Agora RTC Engine and Moved all the dependencies to null safer version

--- lib/pages/call_page.dart
+++ lib/pages/call_page.dart
@@ -1,11 +1,8 @@
 import 'dart:ui';
-
-import 'package:agora_group_calling/widgets/app_bar.dart';
 import 'package:agora_group_calling/widgets/call_function.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:native_screenshot/native_screenshot.dart';
-
 
 class CallPage extends StatefulWidget {
   final String userName;
@@ -19,36 +16,27 @@ class _CallPageState extends State<CallPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-    backgroundColor: Color.fromRGBO(132, 174, 0,0.5),
-    title: Text('Hi '+ widget.userName +'!'),
-    elevation: 10,
-    leading: CircleAvatar(
-      backgroundImage: AssetImage('assets/profile_pic.png'),
-        radius: 10,
-        backgroundColor: Color.fromRGBO(255, 104, 220, 1),
-    ),
-    actions: <Widget>[
-      IconButton(
-        icon: Icon(Icons.image), 
-        onPressed: (){_capturePng();}
-      )
-    ],
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(132, 174, 0, 0.5),
+        title: Text('Hi ' + widget.userName + '!'),
+        elevation: 10,
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/profile_pic.png'),
+          radius: 10,
+          backgroundColor: Color.fromRGBO(255, 104, 220, 1),
         ),
-        body: Stack(
-    // fit: StackFit.expand,
-    children: <Widget>[
-      VideoCall(widget.channelName),
-      Positioned(
-        bottom: 10,
-        left: 60,
-        child: StatusBar(),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.image),
+              onPressed: () {
+                _capturePng();
+              })
+        ],
       ),
-    ],
-        ),
-        
-      );
+      body: VideoCall(widget.channelName),
+    );
   }
+
   Future<void> _capturePng() async {
     String path = await NativeScreenshot.takeScreenshot();
     print(path);
